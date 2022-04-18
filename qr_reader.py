db0296602d14721b1555fc4f7435a5a7bd4d2c41
Jeno Lustyik <lustyik.jeno@gmail.com>
2022-04-18 14:22:50 +0200

    streamlit interface

---
+++ qr_reader.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+import streamlit as st
+from PIL import Image
+
+st.title('QR Code Reader v0.5')
+
+img = st.file_uploader('Upload your QR image here')
+
+
+
+if img is not None:
+    img = Image.open(img)
+    img = img.save('img.jpg')
+    img = cv.imread('img.jpg')
+    qr = cv.QRCodeDetector()
+    x, y, string = qr.detectAndDecode(img)
+
+    st.text(f'Your QR code contains the following:\n{x}')
+
+
